10b2b6bdd780b2634a063771ac89d031d5648dda
Claudiu <clau.claudiu10100@gmail.com>
2024-11-08 23:51:58 +0200

    Almost finished backend

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ const port=3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+let date=new Date();
+let year=date.getFullYear();
+
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.render("home.ejs",{Year:year});
 })
 
+app.get("/home",(req,res)=>{
+    res.render("home.ejs",{Year:year});
+})
+
+app.get("/info",(req,res)=>{
+    res.render("info.ejs",{Year:year});
+})
+
+app.get("/contact",(req,res)=>{
+    res.render("contact.ejs",{Year:year});
+})
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
